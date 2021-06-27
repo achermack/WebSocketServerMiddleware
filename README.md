d8d107a4e4c8e173b92da1e19b03b42c4a13d81b
achermack <achermack@wisc.edu>
2021-06-27 14:27:11 -0500

    :fire: [feat] Logging

--- README.md
+++ README.md
@@ -1,20 +1,35 @@
 # ASP.NET Core WebSocket Middleware
 
-WebSockets with Kestrel made easy
+This middleware uses the System.Net.WebSockets library to easily configure Kestrel to accept incoming WebSocket connections and define handlers for them, similiarly to Fleck
+
+
+# Usage
 
 ``` csharp
-// In your configure method:
-            var server = new WebSocketConnection();
-            server.Start(c =>
+// First, you want to configure the port of your websocket server
+// In Program.cs
+
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:8181");
+                });
+
+// In Startup.cs configure method, you want to configure how each connection is handled by the middleware
+// Inspired by Fleck
+
+            var _newConnection = new WebSocketConnection();
+            _newConnection.Start(c =>
             {
-                c.OnOpen = socket => Console.WriteLine("OnOpen");
-                c.OnClose = socket => Console.WriteLine("OnClose");
-                c.OnMessage = async (socket, message) =>
+                _newConnection.OnOpen = socket => Console.WriteLine("OnOpen");
+                _newConnection.OnClose = socket => Console.WriteLine("OnClose");
+                _newConnection.OnMessage = async (socket, message) =>
                 {
                     Console.WriteLine($"OnMessage: {message}");
-                    //await connection.SendAsync(socket, "hello world");
+                    await _newConnection.SendAsync(socket, $"echo: {message}");
                 };
-                c.OnBinary = (socket, bytes) =>
+                _newConnection.OnBinary = (socket, bytes) =>
                 {
                     Encoding.UTF8.GetString(bytes);
                 };
--- WebSocketMiddleware.cs
+++ WebSocketMiddleware.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
+
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,11 +13,14 @@ namespace websockets_middleware
 {
     public class WebSocketMiddleware
     {
+        private readonly int BUFFER_SIZE = 1024;
         private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
         private readonly WebSocketConnection _connection;
 
-        public WebSocketMiddleware(RequestDelegate next, WebSocketConnection connection)
+        public WebSocketMiddleware(RequestDelegate next,  ILoggerFactory loggerFactory, WebSocketConnection connection)
         {
+            _logger = loggerFactory.CreateLogger<WebSocketMiddleware>();
             _next = next;
             _connection = connection;
         }
@@ -26,7 +31,7 @@ namespace websockets_middleware
                 var socket = await context.WebSockets.AcceptWebSocketAsync();
 
                 _connection.OnOpen(socket);
-
+                _logger.LogInformation($"WebSocket client connected!");
                 await Receive(socket, (result, buffer) =>
                 {
                     HandleMessages(result, buffer, socket);
@@ -35,13 +40,19 @@ namespace websockets_middleware
             await _next(context);
         }
 
+        private string ParseUTF8(string UTF8)
+        {
+            return UTF8.Substring(0, Math.Max(0, UTF8.IndexOf('\0')));
+        }
+
         private void HandleMessages(WebSocketReceiveResult result, byte[] buffer, WebSocket socket)
         {
             switch (result.MessageType)
             {
                 case WebSocketMessageType.Text:
-                    var s = Encoding.UTF8.GetString(buffer);
-                    _connection.OnMessage(socket, s.Substring(0, Math.Max(0, s.IndexOf('\0'))));
+                    var message = ParseUTF8(Encoding.UTF8.GetString(buffer));
+                    _logger.LogInformation($"ClientMessage: {message}");
+                    _connection.OnMessage(socket, message);
                     break;
                 case WebSocketMessageType.Binary:
                     _connection.OnBinary(socket, buffer);
@@ -57,7 +68,8 @@ namespace websockets_middleware
             
             while (socket.State == WebSocketState.Open)
             {
-                var buffer = new byte[1024];
+
+                var buffer = new byte[BUFFER_SIZE];
                 var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), cancellationToken: CancellationToken.None);
 
                 handler(result, buffer);
